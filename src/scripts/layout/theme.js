6fe039cd4ccdabd9af9fabd0a305569a9d2a5400
pebiantara <pebiantara@gmail.com>
2019-02-07 13:37:59 +0700

    cleaned files starter theme

--- src/scripts/layout/theme.js
+++ src/scripts/layout/theme.js
@@ -3,17 +3,16 @@ import '../../styles/theme.scss.liquid';
 
 // Bootstrap v4
 import 'bootstrap/js/dist/util.js';
-import 'bootstrap/js/dist/alert.js';
-import 'bootstrap/js/dist/button.js';
 import 'bootstrap/js/dist/carousel.js';
 import 'bootstrap/js/dist/collapse.js';
-// import 'bootstrap/js/dist/dropdown.js';
 import 'bootstrap/js/dist/modal.js';
-// import 'bootstrap/js/dist/tooltip.js';
-// import 'bootstrap/js/dist/popover.js';
 import 'bootstrap/js/dist/scrollspy.js';
 import 'bootstrap/js/dist/tab.js';
-import 'fontawesome';
+// import 'bootstrap/js/dist/alert.js';
+// import 'bootstrap/js/dist/button.js';
+// import 'bootstrap/js/dist/tooltip.js';
+// import 'bootstrap/js/dist/popover.js';
+// import 'bootstrap/js/dist/dropdown.js';
 
 import 'lazysizes/plugins/object-fit/ls.object-fit';
 import 'lazysizes/plugins/parent-fit/ls.parent-fit';
